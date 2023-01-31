4a3bdc9e8e22cd9d1cdc9654c188e9ffde51042a
pcg7 <peijiun.chong@3ds.com>
2023-01-31 22:58:24 +0800

    update insightmedium

---
+++ shareme_backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '93ze03vq',
+    dataset: 'production'
+  }
+})
---
+++ shareme_backend/schemas/index.js
@@ -0,0 +1,7 @@
+import comment from "./comment";
+import post from "./post";
+import postedBy from "./postedBy";
+import save from "./save";
+import user from "./user";
+
+export const schemaTypes = [user, post, save, comment, postedBy]
