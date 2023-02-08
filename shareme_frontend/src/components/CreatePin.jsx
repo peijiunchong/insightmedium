12b55ae93a02199042d044ba2b9a8a1ac81375f1
peijiun <peijiunchong@gmail.com>
2023-02-08 23:01:41 +0800

    update code create-pin

--- shareme_frontend/src/components/CreatePin.jsx
+++ shareme_frontend/src/components/CreatePin.jsx
@@ -7,7 +7,7 @@ import { client } from '../client'
 import Spinner from './Spinner'
 import { categories } from '../utils/data'
 
-const CreatePin = () => {
+const CreatePin = ({user}) => {
   const [title, setTitle] = useState("");
   const [about, setABout] = useState("");
   const [destination, setDestination] = useState("");
@@ -36,6 +36,37 @@ const CreatePin = () => {
 
     } else {
       setWrongImageType(true)
+    }
+  }
+
+  const savePin = () => {
+    if(title && about && destination && imageAsset?._id && category) {
+      const doc = {
+        _type: 'post',
+        title,
+        about,
+        destination,
+        image: {
+          _type: 'image',
+          asset: {
+            _type: 'reference',
+            _ref: imageAsset?._id
+          }
+        },
+        userId: user._id,
+        postedBy: {
+          _type: 'postedBy',
+          _ref: user._id
+        },
+        category
+      }
+      client.create(doc).then(()=>{
+        navigate("/")
+      })
+    } else {
+      setFields(true);
+
+      setTimeout(() => setFields(false), 2000)
     }
   }
 
@@ -83,6 +114,64 @@ const CreatePin = () => {
               )
             }
           </div>
+        </div>
+        <div className='flex flex-1 flex-col gap-6 lg:pl-5 mt-5 w-full'>
+            <input 
+              type="text" 
+              value={title} 
+              onChange={(e) => setTitle(e.target.value)} 
+              placeholder="Add your title here"
+              className='outline-none text-2xl sm:text-xl font-bold border-b-2 p-2 border-gray-200' 
+            />
+            {
+              user && (
+                <div className='flex items-center gap-2 my-2'>
+                  <img src={user.image} alt="" className='w-10 h-10 rounded-full' />
+                  <p className='font-bold'>{user.userName}</p>
+                </div>
+              )
+            }
+            <input 
+              type="text" 
+              value={about} 
+              onChange={(e) => setABout(e.target.value)} 
+              placeholder="What is your pin about"
+              className='outline-none text-base sm:text-lg border-b-2 p-2 border-gray-200' 
+            />
+
+            <input 
+              type="text" 
+              value={destination} 
+              onChange={(e) => setDestination(e.target.value)} 
+              placeholder="Add a destination link"
+              className='outline-none text-base sm:text-lg border-b-2 p-2 border-gray-200' 
+            />
+
+            <div className='flex flex-col'>
+              <div>
+                <p className='mb-2 font-semibold text-lg sm:text-xl'>Choose Pin Category</p>
+                <select name="" id="" 
+                  onChange={(e) => setCategory(e.target.value)}
+                  className="outline-none w-4/5 text-base border-b-2 border-gray-200 p-2"
+                >
+                  <option value="other" className='bg-white'>Select Category</option>
+                  {
+                    categories.map((category) => (
+                      <option value={category.name} className="text-base border-0 outline-none capitalize bg-white text-black">{category.name}</option>
+                    ))
+                  }
+                </select>
+              </div>
+              <div className='flex justify-end items-end mt-5'>
+                <button 
+                  type='button' 
+                  onClick={savePin} 
+                  className="bg-red-500 text-white rounded-full p-2 w-28 font-bold"
+                >
+                  Save Pin
+                </button>
+              </div>
+            </div>
         </div>
       </div>
     </div>
